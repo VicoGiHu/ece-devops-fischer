b6401a52e0a91f56454c06aa7cbf7e29350bdc1c
VicoGiHu <victor1.fischer@gmail.com>
2024-01-01 14:49:58 +0100

    last push

--- Project/test/app.test.js
+++ Project/test/app.test.js
@@ -1,23 +1,57 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const app = require('../app');
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../app';
 
 chai.use(chaiHttp);
-const expect = chai.expect;
+const { expect } = chai;
 
-describe('Web App API', () => {
-  it('should retrieve all items', async () => {
-    const res = await chai.request(app).get('/items');
+describe('User API', () => {
+  it('should get all users', async () => {
+    const res = await chai.request(app).get('/api/users');
     expect(res).to.have.status(200);
     expect(res.body).to.be.an('array');
   });
 
-  it('should add a new item', async () => {
-    const newItem = { name: 'Test Item', description: 'This is a test item.' };
-    const res = await chai.request(app).post('/items').send(newItem);
+  it('should get a single user by ID', async () => {
+    const userId = 1;
+    const res = await chai.request(app).get(`/api/users/${userId}`);
     expect(res).to.have.status(200);
-    expect(res.body).to.have.property('_id');
-    expect(res.body.name).to.equal(newItem.name);
-    expect(res.body.description).to.equal(newItem.description);
+    expect(res.body).to.be.an('object');
+    expect(res.body.id).to.equal(userId);
+  });
+
+  it('should create a new user', async () => {
+    const newUser = {
+      username: 'vic_fish',
+      email: 'vicfish@example.com',
+    };
+
+    const res = await chai.request(app).post('/api/users').send(newUser);
+    expect(res).to.have.status(201);
+    expect(res.body).to.be.an('object');
+    expect(res.body).to.include(newUser);
+  });
+
+  it('should update an existing user', async () => {
+    const userId = 1;
+    const updatedUser = {
+      username: 'updated_username',
+      email: 'updated_email@example.com',
+    };
+
+    const res = await chai.request(app).put(`/api/users/${userId}`).send(updatedUser);
+    expect(res).to.have.status(200);
+    expect(res.body).to.be.an('object');
+    expect(res.body).to.include(updatedUser);
+  });
+
+  it('should delete an existing user', async () => {
+    const userId = 1;
+
+    const res = await chai.request(app).delete(`/api/users/${userId}`);
+    expect(res).to.have.status(204);
+
+    const getUserRes = await chai.request(app).get(`/api/users/${userId}`);
+    expect(getUserRes).to.have.status(404);
   });
 });
